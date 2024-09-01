9ca68cbff2a955b1858220c407c5f8d8dc3538f1
Sebastian Schaetz <seb@gochromatic.com>
2024-09-01 15:54:40 +0200

    Add support for protobuf

--- src/example_no_ui.py
+++ src/example_no_ui.py
@@ -1,6 +1,8 @@
 import sys
 import numpy as np
 from pathlib import Path
+from src.api_pb2 import ExampleMessage
+from google.protobuf import text_format
 
 # This should really be 
 # `from rules_python.python.runfiles import runfiles`
@@ -8,6 +10,10 @@ from pathlib import Path
 from python.runfiles import runfiles
 
 def run():
+    interpreter_path = sys.executable
+    python_version = f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
+    print(f"Python Interpreter: {interpreter_path}\nPython Version: {python_version}")
+
     # Make sure numpy is working.
     assert np.isclose(1.0 + 41.0, 42.0)
     
@@ -16,11 +22,17 @@ def run():
     rfile = Path(r.Rlocation("_main/src/data.txt"))
     assert "Test File" == rfile.read_text()
 
-    interpreter_path = sys.executable
-    python_version = f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
-    print(f"Python Interpreter: {interpreter_path}\nPython Version: {python_version}")
-    return True
+    # Load a text protobuf:
+    proto_file_path = r.Rlocation("_main/src/api_example.pb.txt")
+    with open(proto_file_path, 'r') as proto_file:
+        proto_content = proto_file.read()
     
+    example_message = ExampleMessage()
+    text_format.Parse(proto_content, example_message)
+    assert example_message.index == 42
+
+    return True
+
 if __name__ == "__main__":
     if not run():
         print("Failed to run example!")
